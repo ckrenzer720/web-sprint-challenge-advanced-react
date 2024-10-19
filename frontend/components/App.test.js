35c34c65edf5eb0ec5dc78157c648bd2ec736fe4
Conner Krenzer <krenzer84@gmail.com>
2024-10-18 20:00:09 -0400

    getNextIndex

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,4 +1,15 @@
+
+
 // Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
-})
+test("sanity", () => {
+  screen.debug();
+});
+
+/*
+  getBy -- will always fail unless the test is 100% accurate --> exactly what you want
+
+  queryBy --> does not fail a test case, just gives a false value if value does not exist
+    only if followed by expect -- true or false
+
+  findBy --> returns and waits for a promise. A combination of both getBy and queryBy -- APIs and async calls
+*/
--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,34 +1,74 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios";
 
 // Suggested initial states
-const initialMessage = ''
-const initialEmail = ''
-const initialSteps = 0
-const initialIndex = 4 // the index the "B" is at
+const initialMessage = "";
+const initialEmail = "";
+const initialSteps = 0;
+const initialIndex = 4; // the index the "B" is at
 
 export default function AppFunctional(props) {
-  // THE FOLLOWING HELPERS ARE JUST RECOMMENDATIONS.
-  // You can delete them and build your own logic from scratch.
+  const [message, setMessage] = useState(initialMessage);
+  const [email, setEmail] = useState(initialEmail);
+  const [steps, setSteps] = useState(initialSteps);
+  const [index, setIndex] = useState(initialIndex);
+  const [coordinates, setCoordinates] = useState({ x: 2, y: 2 });
 
-  function getXY() {
-    // It it not necessary to have a state to track the coordinates.
-    // It's enough to know what index the "B" is at, to be able to calculate them.
+  function getXY(direction) {
+    let x = coordinates.x;
+    let y = coordinates.y;
+
+    switch (direction) {
+      case "left":
+        x = x - 1;
+        break;
+      case "up":
+        y = y - 1;
+        break;
+      case "right":
+        x = x + 1;
+        break;
+      case "down":
+        y = y + 1;
+        break;
+      default:
+        break;
+    }
+    return { x, y };
   }
 
   function getXYMessage() {
-    // It it not necessary to have a state to track the "Coordinates (2, 2)" message for the user.
-    // You can use the `getXY` helper above to obtain the coordinates, and then `getXYMessage`
-    // returns the fully constructed string.
+    const { x, y } = getXY();
+    return `Coordinates (${x}, ${y})`;
   }
 
   function reset() {
     // Use this helper to reset all states to their initial values.
+    setMessage(initialMessage);
+    setEmail(initialEmail);
+    setSteps(initialSteps);
+    setIndex(initialIndex);
+    setCoordinates({ x: 2, y: 2 });
   }
 
   function getNextIndex(direction) {
-    // This helper takes a direction ("left", "up", etc) and calculates what the next index
+    // This helper takes a direction ("left", "up", "right", "down") and calculates what the next index
     // of the "B" would be. If the move is impossible because we are at the edge of the grid,
     // this helper should return the current index unchanged.
+    const x = index % 3;
+    const y = Math.floor(index / 3);
+    switch (direction) {
+      case "left":
+        return x > 0 ? index - 1 : index;
+      case "up":
+        return y > 0 ? index - 3 : index;
+      case "right":
+        return x < 2 ? index + 1 : index;
+      case "down":
+        return y < 2 ? index + 3 : index;
+      default:
+        return index;
+    }
   }
 
   function move(evt) {
@@ -38,10 +78,12 @@ export default function AppFunctional(props) {
 
   function onChange(evt) {
     // You will need this to update the value of the input.
+    setEmail;
   }
 
-  function onSubmit(evt) {
+  function formSubmit(evt) {
     // Use a POST request to send a payload to the server.
+    evt.preventDefault();
   }
 
   return (
@@ -51,13 +93,11 @@ export default function AppFunctional(props) {
         <h3 id="steps">You moved 0 times</h3>
       </div>
       <div id="grid">
-        {
-          [0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
-            <div key={idx} className={`square${idx === 4 ? ' active' : ''}`}>
-              {idx === 4 ? 'B' : null}
-            </div>
-          ))
-        }
+        {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((idx) => (
+          <div key={idx} className={`square${idx === 4 ? " active" : ""}`}>
+            {idx === 4 ? "B" : null}
+          </div>
+        ))}
       </div>
       <div className="info">
         <h3 id="message"></h3>
@@ -67,12 +107,15 @@ export default function AppFunctional(props) {
         <button id="up">UP</button>
         <button id="right">RIGHT</button>
         <button id="down">DOWN</button>
-        <button id="reset">reset</button>
+        <button id="reset" onClick={reset}>
+          reset
+        </button>
       </div>
-      <form>
+      <form onSubmit={formSubmit}>
         <input id="email" type="email" placeholder="type email"></input>
+        {/* // value={setEmail}  */}
         <input id="submit" type="submit"></input>
       </form>
     </div>
-  )
+  );
 }
